54ea3c8be69e3f8ff95d9597eed5d450a1399f93
Zhihua Yu <2569606y@student.gla.ac.uk>
2021-08-02 21:57:08 +0100

    Add the code from chapter 11 in the book "tango_with_django_project"

--- MyITTeamProject/settings.py
+++ MyITTeamProject/settings.py
@@ -29,7 +29,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-LOGIN_URL = 'rango:login'
+# LOGIN_URL = 'rango:login'
+REGISTRATION_OPEN = True
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = 'rango:index'
+LOGIN_URL = 'auth_login'
 
 # Application definition
 
@@ -41,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 ]
 
 MIDDLEWARE = [
