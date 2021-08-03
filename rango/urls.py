8a2a75e8bfffd9cac734ef972ed0f80a50ac3740
Zhihua Yu <2569606y@student.gla.ac.uk>
2021-08-03 17:41:46 +0100

    delete useless file and code

--- rango/urls.py
+++ rango/urls.py
@@ -9,8 +9,5 @@ urlpatterns = [
     path('city/<slug:city_name_slug>/', views.show_scenery, name='show_scenery'),
     path('add_city/', views.add_city, name='add_city'),
     path('city/<slug:city_name_slug>/add_scenery/', views.add_scenery, name='add_scenery'),
-    # path('register/', views.register, name='register'),
-    # path('login/', views.user_login, name='login'),
     path('mypage/', views.mypage, name='mypage'),
-    # path('logout/', views.user_logout, name='logout'),
 ]
